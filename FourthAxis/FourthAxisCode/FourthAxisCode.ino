38f8a2fb34e8f3601bd5aa0db17693896b6681c2
Nick Lee <snickerdude44@gmail.com>
2026-07-22 08:42:19 -0400

    idek honestly

--- FourthAxis/FourthAxisCode/FourthAxisCode.ino
+++ FourthAxis/FourthAxisCode/FourthAxisCode.ino
@@ -5,23 +5,29 @@
 const uint8_t HAND_A = 4;
 const uint8_t HAND_B = 5;
 
+const uint8_t ENC_A = 34;
+const uint8_t ENC_B = 35;
+const uint8_t ENC_Z = 36;
+
 uint8_t state = 0;
 int32_t pos = 0;
 
 keypad Keypad;
 encoder Handwheel(HAND_A, HAND_B);
-
+encoder Encoder(ENC_A, ENC_B, ENC_Z);
 void setup(){
   Serial.begin(9600);
   Keypad.begin();
   Handwheel.begin();
+  Encoder.begin();
   //pinMode(HAND_A, INPUT);
   //pinMode(HAND_B, INPUT);
 }
 
 void loop(){
   delay(1000);
-  Serial.println(Handwheel.getPosition());
+  //Serial.println(Handwheel.getPosition());
+  Serial.println(Encoder.getPosition());
   //Keypad.scan();
   //digitalWrite(12, HIGH);
   for (int i = 0; i < 8; i ++) {
